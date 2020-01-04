a9bf4b14e58491f2a7b5720b8548d6ec172850cc
Md Rayhan <mdrayhan9464@gmail.com>
2020-01-04 14:20:06 +0600

    ..

---
+++ controllers/index.js
@@ -0,0 +1,20 @@
+
+var express = require('express'),
+    router = express.Router();
+
+router.use('/users', require('./users'));
+
+router.get('/', function (req, res) {
+    res.render('index', {title: 'Rayhan'});
+});
+
+router.get('*', function (req, res) {
+    res.status(404).render('error', {
+        title: 'Rayhan', error: {
+            status: 404,
+            stack: 'Not found'
+        }
+    });
+});
+
+module.exports = router;
