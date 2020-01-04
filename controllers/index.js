afdf394d27e5ab3aa65f99735dc6a4c4d6a122c7
Md Rayhan <mdrayhan9464@gmail.com>
2020-01-04 15:19:31 +0600

    new model and controller added by alpha

--- controllers/index.js
+++ controllers/index.js
@@ -3,6 +3,7 @@ var express = require('express'),
     router = express.Router();
 
 router.use('/users', require('./users'));
+router.use('/attendences', require('./attendences'));
 
 router.get('/', function (req, res) {
     res.render('index', {title: 'Rayhan'});
